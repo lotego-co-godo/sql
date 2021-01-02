f01547200c834de2928fc65cd278743e8b790442
MrCoookie <58787541+MrCoookie@users.noreply.github.com>
2021-01-02 23:04:31 +0100

    Update 5.sql

---
+++ zapytania/5.sql
@@ -0,0 +1,15 @@
+select distinct customer.id,first_name,last_name,city from product_category
+inner join product
+on product_category.id = product.category_id
+inner join offer 
+on offer.product_id = product.id
+inner join offer_customer
+on offer_customer.offer_id = offer.id
+inner join sale
+on sale.id = offer_customer.id
+inner join customer
+on customer.id = offer_customer.customer_id
+inner join address
+on address.id = customer.invoice_address_id
+where city like 'Kraków' and product_category.value like 'hardware';
+ 
