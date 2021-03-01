848e8f1005327b0e65a18c219a167eac0b9dcb93
MrCoookie <58787541+MrCoookie@users.noreply.github.com>
2021-03-01 17:43:55 +0100

    Update 3.sql

--- zapytania/3.sql
+++ zapytania/3.sql
@@ -1,4 +1,18 @@
-select first_name, count(customer_id) from customer,offer_customer
-where customer.id = offer_customer.customer_id
-group by first_name
-;
+select id from customer where id IN 
+(
+	SELECT  customer_id
+	AS idOfCustomer from offer_customer inner join sale on offer_customer.id = sale.offer_customer_id
+	GROUP BY customer_id
+	having COUNT(offer_customer.id) = 
+	(
+		SELECT MAX(countOfferCustomerId)
+		FROM 
+		(
+			SELECT COUNT(offer_customer.id) countOfferCustomerId, customer_id
+			AS idOfCustomer from offer_customer inner join sale on offer_customer.id = sale.offer_customer_id
+			GROUP BY customer_id
+		) as minTrn
+	)
+)
+ORDER by last_name DESC
+limit 1;
