39627e8a0b268726f205d6a4de8f57885f9fe1ac
MrCoookie <58787541+MrCoookie@users.noreply.github.com>
2021-03-01 17:45:31 +0100

    Update 10.sql

---
+++ zapytania/10.sql
@@ -0,0 +1,10 @@
+select AVG(offer_price) from offer
+inner join offer_customer
+on offer.id = offer_customer.offer_id
+inner join sale
+on offer_customer.id = sale.offer_customer_id
+inner join customer
+on customer.id = offer_customer.customer_id
+inner join address
+on address.id = customer.invoice_address_id
+where city IN ('Kraków') and offer_price < regular_price*0.9;
